18e6b09d89bcdbf8210fd5b6fe90cffa6f6f55e5
Baris Berisbek <barisberisbekx@gmail.com>
2024-07-23 09:51:27 +0300

    added two apps

---
+++ PasswordAuthAppDemoo1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PasswordAuthAppDemoo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelGeneratedPassword = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.txtEnteredPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelGeneratedPassword
+            // 
+            this.labelGeneratedPassword.AutoSize = true;
+            this.labelGeneratedPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelGeneratedPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelGeneratedPassword.Location = new System.Drawing.Point(47, 62);
+            this.labelGeneratedPassword.Name = "labelGeneratedPassword";
+            this.labelGeneratedPassword.Size = new System.Drawing.Size(121, 40);
+            this.labelGeneratedPassword.TabIndex = 0;
+            this.labelGeneratedPassword.Text = "Şifre : ";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelTimer.Location = new System.Drawing.Point(520, 198);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(150, 31);
+            this.labelTimer.TabIndex = 1;
+            this.labelTimer.Text = "Kalan Süre:";
+            // 
+            // txtEnteredPassword
+            // 
+            this.txtEnteredPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtEnteredPassword.Location = new System.Drawing.Point(520, 62);
+            this.txtEnteredPassword.Name = "txtEnteredPassword";
+            this.txtEnteredPassword.Size = new System.Drawing.Size(237, 45);
+            this.txtEnteredPassword.TabIndex = 2;
+            this.txtEnteredPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLogin.Location = new System.Drawing.Point(619, 127);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(97, 40);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Giriş";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.BackColor = System.Drawing.Color.White;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelResult.ForeColor = System.Drawing.Color.Lime;
+            this.labelResult.Location = new System.Drawing.Point(516, 265);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 20);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PasswordAuthAppDemoo1.Properties.Resources.Adsız_tasarım__4_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(843, 454);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtEnteredPassword);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelGeneratedPassword);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "authenticator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelGeneratedPassword;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.TextBox txtEnteredPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ PasswordAuthAppDemoo1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using OtpNet;
+
+namespace PasswordAuthAppDemoo1
+{
+    public partial class Form1 : Form
+    {
+        private Totp totp1;
+        private Hotp hotp1;
+        private Totp totp2;
+        private Hotp hotp2;
+        private string secretKey1 = "JBSWY3DPEHPK3PXP"; 
+        private string secretKey2 = "KRSXG5A=";
+        private int counter = 0; 
+        private int timeLeft = 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+            GenerateNewPassword();
+        }
+
+        private void GenerateNewPassword()
+        {
+            var base32Bytes1 = Base32Encoding.ToBytes(secretKey1);
+            var base32Bytes2 = Base32Encoding.ToBytes(secretKey2);
+            totp1 = new Totp(base32Bytes1, step: 60);
+            hotp1 = new Hotp(base32Bytes1);
+            totp2 = new Totp(base32Bytes2, step: 60);
+            hotp2 = new Hotp(base32Bytes2);
+
+            string totpPassword1 = Base64Encode(totp1.ComputeTotp());
+            string hotpPassword1 = Base64Encode(hotp1.ComputeHOTP(counter));
+            labelGeneratedPassword.Text = $"TOTP1: {totpPassword1} \nHOTP1: {hotpPassword1}";
+            timeLeft = 60;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                labelTimer.Text = $"Kalan Süre: {timeLeft} sn";
+            }
+            else
+            {
+                GenerateNewPassword();
+                counter++;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var base32Bytes1 = Base32Encoding.ToBytes(secretKey1);
+            var base32Bytes2 = Base32Encoding.ToBytes(secretKey2);
+            totp1 = new Totp(base32Bytes1, step: 60);
+            hotp1 = new Hotp(base32Bytes1);
+            totp2 = new Totp(base32Bytes2, step: 60);
+            hotp2 = new Hotp(base32Bytes2);
+
+            string enteredPassword = txtEnteredPassword.Text;
+            bool isTotp1Valid = VerifyBase64Totp(totp1, enteredPassword);
+            bool isHotp1Valid = VerifyBase64Hotp(hotp1, enteredPassword);
+            bool isTotp2Valid = VerifyBase64Totp(totp2, enteredPassword);
+            bool isHotp2Valid = VerifyBase64Hotp(hotp2, enteredPassword);
+
+            if (isTotp1Valid || isTotp2Valid)
+            {
+                labelResult.Text = "Doğru TOTP şifresi";
+                labelResult.ForeColor = System.Drawing.Color.Green;
+            }
+            else if (isHotp1Valid || isHotp2Valid)
+            {
+                labelResult.Text = "Doğru HOTP şifresi";
+                labelResult.ForeColor = System.Drawing.Color.Green;
+                counter++;
+            }
+            else
+            {
+                labelResult.Text = "Yanlış şifre";
+                labelResult.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private bool VerifyBase64Totp(Totp totp, string base64Totp)
+        {
+            string decodedTotp = Base64Decode(base64Totp);
+            var verificationWindow = new VerificationWindow(previous: 1, future: 1);
+            return totp.VerifyTotp(decodedTotp, out long timeStepMatched, verificationWindow);
+        }
+
+        private bool VerifyBase64Hotp(Hotp hotp, string base64Hotp)
+        {
+            string decodedHotp = Base64Decode(base64Hotp);
+            for (int i = -1; i <= 1; i++)
+            {
+                if (hotp.VerifyHotp(decodedHotp, counter + i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        private string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        private void labelResult_Click(object sender, EventArgs e)
+        {
+            //null
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //null
+        }
+    }
+}
